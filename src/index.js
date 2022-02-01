3159cc7047665db8ed3011a7b52e2f9e1b3dcac4
dayanersilva <90991499+dayanersilva@users.noreply.github.com>
2022-02-01 20:55:31 -0300

    mostrando o resultado das funçoes para o usuário na tela

--- src/index.js
+++ src/index.js
@@ -3,26 +3,38 @@ import validator from './validator.js';
 
 //console.log(validator.isValid(numeroCartao))
 
-
+const numcartao = document.getElementById('numcartao')
 const validar = document.getElementById('validarCartao')
+let msg = document.getElementById('msg')
+
+numcartao.addEventListener('click', masknum )
 validar.addEventListener('click', clicar)
+
+
+function masknum(){
+    
+    let mask = document.getElementById('numcartao').value
+    let maskNum = validator.maskify(mask)
+
+    msg.textContent = maskNum
+    
+}
+
 
 function clicar() {
     let numeroCartao = document.getElementById('numcartao').value
-
-    if(numeroCartao === '') {
-        alert ('Nenhum número foi digitado')
-        return
-    }
-
     let numeroevalido = validator.isValid(numeroCartao)
     
     if(numeroevalido === true){
-       alert ('É válido')
+
+        msg.textContent = "Cartão Válido" 
+       //alert ('Cartão é válido')
     }else{
-        alert ('Não é válido') 
+
+        msg.textContent = "Cartão não é válido"
+        //alert ('Cartão não é válido') 
     }
 
-    validator.maskify(numeroCartao)
+    //validator.maskify(numeroCartao)
 
 }
