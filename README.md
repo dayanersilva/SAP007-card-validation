6fc7b587cbed1a99b5d40148e53c89b4acf9ec6a
dayanersilva <dayane.rodrigues_silva@hotmail.com>
2022-02-08 09:08:24 -0300

    Incluindo a foto do prototipo no README

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Cartão de Crédito Válido
 
 
-   **Introdução**
+   ## INTRODUÇÃO 
 O algoritmo de Luhn, também chamado de módulo 10, é um método de soma de verificação, usado para validar números de identificação, como o IMEI de telefones celulares, cartões de crédito etc.
 
 Esse algoritmo é simples. Obtemos o inverso do número a ser verificado (que contém apenas dígitos [0-9]); todos os números que ocupam uma posição par devem ser multiplicados por dois; se esse número for maior ou igual a 10, devemos adicionar os dígitos do resultado; o número a verificar será válido se a soma de seus dígitos finais for um múltiplo de 10.
@@ -9,7 +9,7 @@ Esse algoritmo é simples. Obtemos o inverso do número a ser verificado (que co
 ![alt text](https://camo.githubusercontent.com/a2f75e73d94d0c9c7475296b056bdf3219e13a52cd99368c67cdbcccc3af4573/68747470733a2f2f7777772e313031636f6d707574696e672e6e65742f77702f77702d636f6e74656e742f75706c6f6164732f4c75686e2d416c676f726974686d2e706e67)
 
 
-   **Resumo do projeto**
+   ## Resumo do projeto
 Neste projeto, você precisará criar um aplicativo da Web que permita ao usuário validar o número de um cartão de crédito. Além disso, você precisará implementar a funcionalidade para ocultar todos os dígitos de um cartão, exceto os quatro últimos.
 
 Inicialmente procurei identificar e definir um cenário para colocar o projeto do card.
@@ -19,3 +19,9 @@ Nosso objetivo e facilitar e agilizar para o nosso cliente a emissão do cartão
 uma numeração válida.
 Com essa etapa de validação tercerizada, o tempo que o cliente final espera para receber seu cartao em casa é reduzido. 
 Para o usuário final proejetei uma interface simples e de facil compreensão, um layout com tons de azul e figuras deixando o site visualmente mais atraente de olhar e ler seu conteúdo, escrevo um pouco sobre a empresa , como funciona o processo de validação do número do cartão.
+
+## LINKS
+
+[Protótipo simples](card-prototipo.jpeg)
+
+[Deploy](https://dayanersilva.github.io/SAP007-card-validation/)
--- src/validator.js
+++ src/validator.js
@@ -4,23 +4,23 @@ const validator = {
 
   isValid(numeroCartao) {
 
-    let arrayreverso = [] // objeto vazio 
-    for(let i = numeroCartao.length - 1; i >= 0; i--){ // loop ao contrário que pega da direita p esquerda
-        const numero = Number(numeroCartao[i]) // variavel que recebe o valor do for e trasnforma em numero
-        arrayreverso.push(numero) // aqui eu o valor em numero e puxo p o array reverso
+    let arrayreverso = []  
+    for(let i = numeroCartao.length - 1; i >= 0; i--){ 
+        const numero = Number(numeroCartao[i]) 
+        arrayreverso.push(numero) 
     }
 
     let soma = 0
 
-    for (let i = 0; i < arrayreverso.length; i++) { // novo loop que percorre o array invertido
-      if (i % 2 != 0) { // se o numero for impar 
-         arrayreverso[i] *= 2 // multiplica por 2
+    for (let i = 0; i < arrayreverso.length; i++) { 
+      if (i % 2 != 0) { 
+         arrayreverso[i] *= 2 
 
-        if (arrayreverso[i] > 9) { // aqui compara se o numero multiplicado foi maior que 9
-          arrayreverso[i] -= 9 // se for maior que 9 ele subtrai 9
-          soma += arrayreverso[i] // aqui soma
+        if (arrayreverso[i] > 9) { 
+          arrayreverso[i] -= 9 
+          soma += arrayreverso[i] 
         } else {
-          soma += arrayreverso[i] // se o numero nao for par ele vai somando na variavel soma junto com os impares que fizeram todo o processo do if
+          soma += arrayreverso[i] 
         }
       } else {
         soma += arrayreverso[i]
@@ -28,7 +28,7 @@ const validator = {
 
     }
 
-    if (soma % 10 === 0) { // aqui ele vez se o numero e divisivel por 10
+    if (soma % 10 === 0) { 
       return arrayreverso = true
     } else {
       return arrayreverso = false
@@ -36,11 +36,11 @@ const validator = {
   }
   
  ,maskify(numeroCartao) {
-    let digit_mask = Array.from(numeroCartao) // trasforma o valor do input em array 
+    let digit_mask = Array.from(numeroCartao) 
     for (let index = 0; index < digit_mask.length - 4; index++) { 
-      digit_mask[index] = "#" // loop onde os ultimos 4 caracteres nao seram mudados para #
+      digit_mask[index] = "#" 
     }
-    let join = digit_mask.join("") // join junta tudo em uma unica string
+    let join = digit_mask.join("") 
 
     return join
   }
