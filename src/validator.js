487a739342b6e6bd9e2bbbcc428809eca0c4d7ed
dayanersilva <dayane.rodrigues_silva@hotmail.com>
2022-02-08 22:08:20 -0300

    .

--- src/validator.js
+++ src/validator.js
@@ -4,47 +4,41 @@ const validator = {
 
   isValid(numeroCartao) {
 
-    let arrayreverso = []  
-    for(let i = numeroCartao.length - 1; i >= 0; i--){ 
-        const numero = Number(numeroCartao[i]) 
-        arrayreverso.push(numero) 
+    let arrayreverso = []
+    for (let i = numeroCartao.length - 1; i >= 0; i--) {
+      const numero = Number(numeroCartao[i])
+      arrayreverso.push(numero)
     }
 
     let soma = 0
 
-    for (let i = 0; i < arrayreverso.length; i++) { 
-      if (i % 2 != 0) { 
-         arrayreverso[i] *= 2 
+    for (let i = 0; i < arrayreverso.length; i++) {
+      if (i % 2 != 0) {
+        arrayreverso[i] *= 2
 
-        if (arrayreverso[i] > 9) { 
-          arrayreverso[i] -= 9 
-          soma += arrayreverso[i] 
-        } else {
-          soma += arrayreverso[i] 
+        if (arrayreverso[i] > 9) {
+          arrayreverso[i] -= 9
         }
-      } else {
-        soma += arrayreverso[i]
       }
-
+      soma += arrayreverso[i]
     }
 
-    if (soma % 10 === 0) { 
-      return arrayreverso = true
+    if (soma % 10 === 0) {
+      return true
     } else {
-      return arrayreverso = false
+      return false
     }
-  }
-  
- ,maskify(numeroCartao) {
-    let digit_mask = Array.from(numeroCartao) 
-    for (let index = 0; index < digit_mask.length - 4; index++) { 
-      digit_mask[index] = "#" 
+  },
+
+  maskify(numeroCartao) {
+    let digit_mask = Array.from(numeroCartao)
+    for (let index = 0; index < digit_mask.length - 4; index++) {
+      digit_mask[index] = "#"
     }
-    let join = digit_mask.join("") 
+    let join = digit_mask.join("")
 
     return join
   }
 
 }
-
 export default validator;
